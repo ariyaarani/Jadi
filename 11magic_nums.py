b4ce0eedaaeb0f62467d2fe70190a39f999a6724
MOHAMMAD REZA ARANI <ariya.arani82@gmail.com>
2025-04-30 18:45:39 +0330

    Add files via upload

---
+++ 11magic_nums.py
@@ -0,0 +1,13 @@
+
+#Author : Mohammad Reza Arani
+
+n = int(input())
+
+if n % 3 == 0 and n % 5 == 0:
+    print("افسانه ای")
+elif n % 3 == 0:
+    print("جادویی")
+elif n % 5 == 0:
+    print("نفرین شده")
+else:
+    print("معمولی")
